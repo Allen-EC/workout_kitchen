bcf840becb48f97c171be1c59b3ded3d059df02f
1Eva1987 <115692772+1Eva1987@users.noreply.github.com>
2023-02-03 11:24:18 +0000

    API call tested needed data received

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,20 @@
 var recipesDivEl = $(".recipe-cards-container");
 var ingredient = "beef";
 var keyRecipes = "&apiKey=f98ddadddf8c48bc87d34611c1e22683";
-var urlIngredient =
-  "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" +
-  ingredient +
-  "&number=6" +
-  keyRecipes;
+
+// click event on recipes div/latter to be changed on "button"
 recipesDivEl.on("click", function (e) {
   e.preventDefault();
-  //   recipesDivEl.empty();
-  console.log("clicked");
+  recipesDivEl.empty();
+  var urlIngredient =
+    "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" +
+    ingredient +
+    "&number=6" +
+    keyRecipes;
+  $.ajax({
+    url: urlIngredient,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+  });
 });
