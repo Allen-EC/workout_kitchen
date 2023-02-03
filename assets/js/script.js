d0a2728062201844b2eaddc26bc083732975d15d
1Eva1987 <115692772+1Eva1987@users.noreply.github.com>
2023-02-03 13:02:51 +0000

    card body element created with title of the recipe and button, card body added to card element. Displaying recipe cards to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,52 @@
-var recipesDivEl = $(".recipe-cards-container");
+var recipesContainerEl = $(".recipe-cards-container");
 var ingredient = "beef";
 var keyRecipes = "&apiKey=f98ddadddf8c48bc87d34611c1e22683";
 
-// click event on recipes div/latter to be changed on "button"
-recipesDivEl.on("click", function (e) {
+// click event on recipes div-container/latter to be changed on "button"
+recipesContainerEl.on("click", function (e) {
   e.preventDefault();
-  recipesDivEl.empty();
+  recipesContainerEl.empty();
   var urlIngredient =
     "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" +
     ingredient +
     "&number=6" +
     keyRecipes;
+  // API call
   $.ajax({
     url: urlIngredient,
     method: "GET",
   }).then(function (response) {
-    console.log(response);
     // loop over the response array and dynamically create recipe card based on received data
     for (var i = 0; i < response.length; i++) {
-      var divElCard = $("<div>");
-      divElCard.addClass("card");
-      divElCard.attr("style", "width:20rem");
+      // creatin card div
+      var divRecipeCard = $("<div>");
+      divRecipeCard.addClass("card");
+      divRecipeCard.attr("style", "width:20rem");
+      // creating image
       var imageRecipe = $("<img>");
       imageRecipe.addClass("card-img-top");
       imageRecipe.attr("src", response[i].image);
-      divElCard.append(imageRecipe);
-      recipesDivEl.append(divElCard);
+      // creating div (card body) for recipe text and button
+      var recipeCardBody = $("<div>");
+      recipeCardBody.addClass("card-body");
+      var recipeTitleEL = $("<p>" + response[i].title + "</p>");
+      var btnFullRecipe = $("<button>See Full Recipe</button>");
+      btnFullRecipe.addClass("btn btn-primary");
+      btnFullRecipe.attr("data-id", response[i].id);
+      // displaying dynamically created cards to the user
+      divRecipeCard.append(imageRecipe, recipeCardBody);
+      recipeCardBody.append(recipeTitleEL, btnFullRecipe);
+      recipesContainerEl.append(divRecipeCard);
     }
   });
 });
+// example of bootstrap card
+
+/* <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div> */
