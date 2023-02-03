281c19fb26148ddaa80e84c17e8c1423f2d3a184
Hayley <86895234+codenamehaylz@users.noreply.github.com>
2023-02-03 19:42:38 +0000

    API call responds to selected options

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,27 @@
-$( "#difficulty" ).selectmenu();
+var targetSelected = function(menu){
+    var selected = menu.children("option:selected").val();
+    return selected;
+}
+
+var ninjaAPI = 'i+Zes/WnU72HDYemlL23/g==UqTBhzMEI1nSnXHh';
+
+
+$('#submit-exercise').on("click", function(){
+
+    var difficulty = targetSelected($('#difficulty'));
+    var type = targetSelected($('#type'));
+
+    $.ajax({
+        method: 'GET',
+        url: 'https://api.api-ninjas.com/v1/exercises?difficulty=' + difficulty + '&type=' + type,
+        headers: { 'X-Api-Key': ninjaAPI},
+        contentType: 'application/json',
+        success: function(result) {
+            console.log(result);
+        },
+        error: function ajaxError(jqXHR) {
+            console.error('Error: ', jqXHR.responseText);
+        }
+    });
+
+})
