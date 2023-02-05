390cb5c35e25998a20904f303870962d393b3e88
1Eva1987 <115692772+1Eva1987@users.noreply.github.com>
2023-02-05 11:11:41 +0000

    comented out part of the code that will be changed and to make sure it would not effect main code on the main branch

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,45 +8,45 @@ var btnSave = $("<button>Save</button>");
 btnSave.addClass("btn btn-secondary");
 
 // click event on recipes div-container/latter to be changed on "button"
-navEl.on("click", function () {
-  recipesContainerEl.empty();
-  var urlIngredient =
-    "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" +
-    ingredient +
-    "&number=6" +
-    keyRecipes;
-  // API call
-  $.ajax({
-    url: urlIngredient,
-    method: "GET",
-  }).then(function (response) {
-    // loop over the response array and dynamically create recipe card based on received data
-    for (var i = 0; i < response.length; i++) {
-      // creatin card div
-      var divRecipeCard = $("<div>");
-      divRecipeCard.addClass("card");
-      divRecipeCard.attr("style", "width:20rem");
-      // creating image
-      var imageRecipe = $("<img>");
-      imageRecipe.addClass("card-img-top");
-      imageRecipe.attr("src", response[i].image);
-      // creating div (card body) for recipe text and button
-      var recipeCardBody = $("<div>");
-      recipeCardBody.addClass("card-body");
-      var recipeTitleEL = $("<h5>" + response[i].title + "</h5>");
-      recipeTitleEL.addClass("card-title");
-      var btnFullRecipe = $("<button>See Full Recipe</button>");
-      btnFullRecipe.addClass("btn btn-primary");
-      btnFullRecipe.attr("data-id", response[i].id);
-      var save = $("<button>Save</button>");
-      save.addClass(" btn btn-secondary");
-      // displaying dynamically created cards to the user
-      recipeCardBody.append(recipeTitleEL, btnFullRecipe, save);
-      divRecipeCard.append(imageRecipe, recipeCardBody);
-      recipesContainerEl.append(divRecipeCard);
-    }
-  });
-});
+// navEl.on("click", function () {
+//   recipesContainerEl.empty();
+//   var urlIngredient =
+//     "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" +
+//     ingredient +
+//     "&number=6" +
+//     keyRecipes;
+//   // API call
+//   $.ajax({
+//     url: urlIngredient,
+//     method: "GET",
+//   }).then(function (response) {
+//     // loop over the response array and dynamically create recipe card based on received data
+//     for (var i = 0; i < response.length; i++) {
+//       // creatin card div
+//       var divRecipeCard = $("<div>");
+//       divRecipeCard.addClass("card");
+//       divRecipeCard.attr("style", "width:20rem");
+//       // creating image
+//       var imageRecipe = $("<img>");
+//       imageRecipe.addClass("card-img-top");
+//       imageRecipe.attr("src", response[i].image);
+//       // creating div (card body) for recipe text and button
+//       var recipeCardBody = $("<div>");
+//       recipeCardBody.addClass("card-body");
+//       var recipeTitleEL = $("<h5>" + response[i].title + "</h5>");
+//       recipeTitleEL.addClass("card-title");
+//       var btnFullRecipe = $("<button>See Full Recipe</button>");
+//       btnFullRecipe.addClass("btn btn-primary");
+//       btnFullRecipe.attr("data-id", response[i].id);
+//       var save = $("<button>Save</button>");
+//       save.addClass(" btn btn-secondary");
+//       // displaying dynamically created cards to the user
+//       recipeCardBody.append(recipeTitleEL, btnFullRecipe, save);
+//       divRecipeCard.append(imageRecipe, recipeCardBody);
+//       recipesContainerEl.append(divRecipeCard);
+//     }
+//   });
+// });
 
 // Click event on "See Full Recipe" button
 recipesContainerEl.on("click", ".btn-primary", function (e) {
